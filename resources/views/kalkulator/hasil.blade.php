0d532daf0a1a3ce581e8fe64279be0e19ffb1244
wikalsnri <wikalsnri@gmail.com>
2024-04-23 08:59:08 +0700

    tes

--- resources/views/kalkulator/hasil.blade.php
+++ resources/views/kalkulator/hasil.blade.php
@@ -6,8 +6,6 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
     <style>
-        /* Tambahkan gaya kustom di sini jika diperlukan */
-
         .container {
             background-color: #f8f9fa;
             border-radius: 10px;
